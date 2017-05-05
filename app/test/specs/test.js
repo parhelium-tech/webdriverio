be7b42e772bb9aab49c7ea661b72d33a773e2079
Michał Dulemba <photocoffeeman@gmail.com>
2017-05-05 11:11:46 +0200

    Update test.js

--- app/test/specs/test.js
+++ app/test/specs/test.js
@@ -2,6 +2,7 @@ var assert = require('assert');
 
 describe('my awesome website', function() {
     it('should do some chai assertions', function() {
+        browser.pause(10000)
         browser.url('/');
         browser.getTitle().should.be.equal('CS Docs');
     });
